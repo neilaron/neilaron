1783b801191f6c59d3cf337780fbfdeec32ce4e6
Neil Aron Pascual <144456101+neilaron@users.noreply.github.com>
2023-09-26 16:17:06 +0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,25 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_1/screens/RollDice.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(RollDiceApp());
+}
+
+class RollDiceApp extends StatelessWidget {
+  const RollDiceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue.shade400,
-          title: Text("My Personal Information"),
-        ),
-        backgroundColor: Colors.blue.shade700,
-        body: SafeArea(
-          child: const Column(
-            children: [
-              Text("Neil Aron P. Pascual"),
-              Text("Pobalcion Balunago, Pangasinan"),
-              Text("Think No Limits")
-            ],
-          ),
-        ),
-      ),
-    ),
-  );
+      home: RollDiceScreen(),
+    );
+  }
 }
